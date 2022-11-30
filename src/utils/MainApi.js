64ef080268509437e78c3fb25f5f3291fdc635fa
Maxim G <golubms2010@yandex.ru>
2022-11-30 22:59:58 +0400

    правки для работы на ВМ

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -102,7 +102,7 @@ class MainApi {
 }
  
 const mainApi = new MainApi({
-    url: 'http://localhost:6688',
+    url: 'https://api.maxflying.diploma.nomoredomains.icu',
     headers: {
         'Content-Type': 'application/json',
     }
